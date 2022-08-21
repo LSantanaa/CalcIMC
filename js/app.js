6f56d1b05317ade485f88e1cd848deb055c88da7
LSantanaa <leonardodesantana05@gmail.com>
2022-08-21 16:41:38 -0300

    upload projeto

---
+++ js/app.js
@@ -0,0 +1,65 @@
+function start() {
+  const btn = document.querySelector('#btn');
+  const weight = document.querySelector('#weight');
+  const height = document.querySelector('#height');
+
+  weight.addEventListener('input', handleBtnClick);
+  height.addEventListener('input', handleBtnClick);
+  btn.addEventListener('click', handleBtnClick);
+  handleBtnClick();
+}
+
+function calcIMC(weight, height) {
+  return weight / (height * height);
+}
+
+function handleBtnClick() {
+  const weight = document.querySelector('#weight');
+  const height = document.querySelector('#height');
+  const imcResult = document.querySelector('#imcResult');
+  const imcFinal = document.querySelector('#imcFinal');
+
+  let valueHeight = Number(height.value);
+  let valueWeight = Number(weight.value);
+
+  let imc = calcIMC(valueWeight, valueHeight).toFixed(2);
+  let classificacaoImc = '';
+
+  if (imc < 16) {
+    classificacaoImc = 'Não há classificação, valores inválidos.';
+  } else if (imc >= 16 && imc <= 16.99) {
+    classificacaoImc = 'Muito abaixo do peso';
+    imcResult.className = 'red';
+    imcFinal.className = 'red';
+  } else if (imc >= 17 && imc <= 18.49) {
+    classificacaoImc = 'Abaixo do peso';
+    imcResult.className = 'yellow';
+    imcFinal.className = 'yellow';
+  } else if (imc >= 18.5 && imc <= 24.99) {
+    classificacaoImc = 'Peso normal';
+    imcResult.className = 'green';
+    imcFinal.className = 'green';
+  } else if (imc >= 25 && imc <= 29.99) {
+    classificacaoImc = 'Acima do peso';
+    imcResult.className = 'yellow';
+    imcFinal.className = 'yellow';
+  } else if (imc >= 30 && imc <= 34.99) {
+    classificacaoImc = 'Obesidade grau I';
+    imcResult.className = 'yellow';
+    imcFinal.className = 'yellow';
+  } else if (imc >= 35 && imc <= 40) {
+    classificacaoImc = 'Obesidade grau II';
+    imcResult.className = 'red';
+    imcFinal.className = 'red';
+  } else if (imc >= 40) {
+    classificacaoImc = 'Obesidade grau III';
+    imcResult.className = 'red';
+    imcFinal.className = 'red';
+  }
+  imc = imc.replace('.', ',');
+
+  imcResult.textContent = imc;
+  imcFinal.textContent = classificacaoImc;
+}
+
+start();
---
+++ readme.md
@@ -0,0 +1,23 @@
+## Cálcule seu índice de massa corporal(IMC)
+
+#### Olá, você sabe o que é o IMC?
+
+O IMC é um índice que mede se você está abaixo, dentro ou acima do peso, de acordo com a relação entre seu peso e altura. Descubra como calcular esse índice, quais os valores ideais, as exceções da tabela padrão, a especificidade do IMC para crianças e jovens e muito mais neste artigo completo.
+
+## Cálculo do Índice de Massa Corporal
+
+Para a sua posição na tabela do Índice de Massa Corporal, você precisa de dois valores: seu peso e altura. É só dividir o peso (em quilos) pelo quadrado da altura (em metros), que é obtido pela multiplicação da altura por ela mesma.
+
+Então, a fórmula fica assim: **IMC = Peso** **/ Altura².**
+
+## Exceções do IMC
+
+Vários grupos possuem características específicas e não se encaixam na tabela do IMC tradicional, como atletas e grávidas. Os primeiros possuem alta quantidade de massa muscular, o que resulta em um IMC acima do considerado normal, mas não significa que eles estejam acima do peso. As gestantes também podem possuir um IMC elevado, devido ao peso do bebê.
+
+## Limitações do IMC
+
+Apesar de ser um dos parâmetros mais utilizados para se calcular o peso ideal, a tabela do IMC não considera diversas variáveis importantes no que diz respeito a uma vida saudável – como porcentagem de gordura visceral, o teor de massa magra e a prática de atividades físicas. Então, o ideal é utilizar outros índices e parâmetros juntamente com a tabela do IMC para obter uma avaliação adequada.,
+
+#### Fonte : <a hrer='https://qbemqfaz.com.br/vida-equilibrada/tabela-imc?gclid=EAIaIQobChMIsPi3rNbY-QIV2uFcCh1SfAiiEAAYASAAEgJj6fD_BwE'>Que Bem Que Faz
+
+##### A tabela IMC usada neste projeto foi do site UOL, que você encontra aqui: <a href='https://www.uol.com.br/vivabem/faq/imc-como-calcular-tabela-dicas-como-melhorar-e-mais.htm'>Viva Bem Uol
